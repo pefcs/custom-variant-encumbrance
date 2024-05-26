9ddb257f01cdf1b67941e42499500a75e36f5729
pefcs <118229534+pefcs@users.noreply.github.com>
2024-05-26 05:50:33 +0200

    Check containers equip and remove encumbrance weight
    Remove weight for items in an unequipped container, in case the item itself is not equipped.

--- scripts/encumbrance.js
+++ scripts/encumbrance.js
@@ -58,8 +58,15 @@ Hooks.once('init', () => {
         const itemWeight = getItemWeight(item);
         const itemQuantity = item.system.quantity || 0;
         const equipped = item.system.equipped || false;
+        const inContainer = item.system.container != null;
+	let containerEquipped = false;
 
-        if (equipped) {
+        if (inContainer) {
+          const container = this.items.get(item.system.container);
+          containerEquipped = container ? container.system.equipped : false;
+        }
+	      
+        if (equipped || (inContainer && containerEquipped)) {
           totalWeight += itemWeight * itemQuantity;
         }
       });
@@ -166,7 +173,15 @@ function updateActorEncumbrance(actor) {
   let totalWeight = actor.items.reduce((acc, item) => {
     const itemWeight = getItemWeight(item);
     const itemQuantity = item.system.quantity || 0;
-    if (item.system.equipped || countUnequippedItems) {
+    const inContainer = item.system.container != null;
+    let containerEquipped = false;
+
+    if (inContainer) {
+      const container = actor.items.get(item.system.container);
+      containerEquipped = container ? container.system.equipped : false;
+    }
+	  
+    if (item.system.equipped || countUnequippedItems || (inContainer && containerEquipped)) {
       return acc + (itemWeight * itemQuantity);
     }
     return acc;
